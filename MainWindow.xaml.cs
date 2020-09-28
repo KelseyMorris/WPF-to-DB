100aae6e16b08f2c9bf4c394f7a4841605f4e654
KelseyMorris <70164125+KelseyMorris@users.noreply.github.com>
2020-09-28 12:42:29 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_to_DB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OleDbConnection cn;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string query = “select* from Assets”;
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = “”;
+            while (read.Read())
+            {
+                data += read[0].ToString() + “\n”;
+
+            }
+        }
+    }
+}
